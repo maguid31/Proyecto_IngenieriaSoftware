3e1c3e55623c7627da527aafb12351a10f7e7800
Milagros Rivero <riveromilagros820@gmail.com>
2026-04-25 16:47:35 -0300

    BLLbitacora

--- BLL/BitacoraBLL_65RD.cs
+++ BLL/BitacoraBLL_65RD.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,15 @@ using System.Threading.Tasks;
 
 namespace BLL
 {
-    internal class BitacoraBLL_65RD
+    public class BitacoraBLL_65RD
     {
+            private BitacoraDAL_65RD _bitacoraDAL = new BitacoraDAL_65RD();
+
+            public void RegistrarEvento(int usuarioId, string accion, string descripcion)
+            {
+                _bitacoraDAL.RegistrarAccion(usuarioId, accion, descripcion);
+            }
+        
+
     }
 }
